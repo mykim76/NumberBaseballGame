17435a7305ee717fb009d967827634ca4ac837bb
mykim76 <iyoungfm@gmail.com>
2020-06-11 20:46:57 +0900

    실제 채팅 내용을 상황에 따라 다른 텍스트뷰에 출력

--- app/src/main/java/com/example/numberbaseballgame/adapters/ChatAdapter.kt
+++ app/src/main/java/com/example/numberbaseballgame/adapters/ChatAdapter.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.numberbaseballgame.R
 import com.example.numberbaseballgame.datas.Chat
+import org.w3c.dom.Text
 
 class ChatAdapter(val mContext: Context, val resId:Int, val mList:List<Chat>) : ArrayAdapter<Chat>(mContext, resId, mList) {
     val inf = LayoutInflater.from(mContext)
@@ -26,18 +28,25 @@ class ChatAdapter(val mContext: Context, val resId:Int, val mList:List<Chat>) : 
         val chatComputerLayout = row.findViewById<LinearLayout>(R.id.computerChatLayout)
         val chatUserLayout = row.findViewById<LinearLayout>(R.id.userChatLayout)
 
+        val computerChatTxt = row.findViewById<TextView>(R.id.computerChatTxt)
+        val userChatTxt = row.findViewById<TextView>(R.id.userChatTxt)
+
         val data = mList[position]
         // 컴퓨터가 말할 때와 사람이 말할 때를 구분하여 표시
         when(data.who){
             "USER" ->{
                 chatUserLayout.visibility = View.VISIBLE
                 chatComputerLayout.visibility =View.GONE
+
+                userChatTxt.text = data.content
                 
             }
             "CPU" -> {
 
                 chatUserLayout.visibility = View.GONE
                 chatComputerLayout.visibility =View.VISIBLE
+
+                computerChatTxt.text = data.content
             }
             else -> {
                 Log.e("에러발생","사용자/컴퓨터와의 화자 생김")
