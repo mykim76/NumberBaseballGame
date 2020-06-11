1e9d179f5778ab101612ea1b3a315d7c97fd0549
mykim76 <iyoungfm@gmail.com>
2020-06-11 19:48:59 +0900

    컴퓨터 문제 출제 로직 작성중  - 구조: 3개의 숫자 뽑기 => 각 숫자는 조건에 맞을때 까지 무한 반복

--- app/src/main/java/com/example/numberbaseballgame/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseballgame/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class MainActivity : BaseActivity() {
 
+    //컴퓨터가 낸 문제 숫자 세 개를 저장할 ArrayList
+    val computerNumbers = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,10 +16,39 @@ class MainActivity : BaseActivity() {
         setupEvents()
     }
     override fun setValues() {
-
+//문제를 내라고 컴퓨터에게 시킴 =>문제:3자리 숫자 배열
+        makeComputerNumber()
     }
 
     override fun setupEvents() {
 
+    }
+    
+    fun makeComputerNumber()
+    {
+        //숫자 세 개를 랜덤 생성=>3번 반복
+        for(i in 0..2)
+        {
+            //규칙에 맞는 숫자를 뽑을때 까지 무한 반복
+            while(true) {
+
+                // TODO : 1~9 숫자 랜덤으로 뽑기
+                
+                
+                //숫자가 유효한지 저장할 변수
+                var isNumberOK = true
+
+
+                // TODO : 숫자를 써도 되는지 검사
+                
+                //조건에 맞는 숫자가 찾아지면 무한반복 맘춤
+                if(isNumberOK)
+                {
+                    break
+                }
+
+            }
+            computerNumbers.add()
+        }
     }
 }
